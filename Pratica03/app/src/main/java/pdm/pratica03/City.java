b611dddc6334563a1d321a07f30a93c996e9508f
Nelson Henrique <nhsneto@gmail.com>
2023-05-24 06:16:28 -0300

    Adiciona pratica 3

---
+++ Pratica03/app/src/main/java/pdm/pratica03/City.java
@@ -0,0 +1,20 @@
+package pdm.pratica03;
+
+public class City {
+
+    private String name;
+    private String info;
+
+    public City(String name, String info) {
+        this.name = name;
+        this.info = info;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+}

4bcfa369bac8efe77a1ad72288180b8b3119c5d4
Nelson Henrique <nhsneto@gmail.com>
2023-07-09 23:56:55 -0300

    Adiciona pratica 8

---
+++ Pratica08/app/src/main/java/pdm/nelson/pratica07/ForecastActivity.java
@@ -0,0 +1,36 @@
+package pdm.nelson.pratica07;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class ForecastActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forecast);
+        LinearLayout layout = findViewById(R.id.layout_forecast);
+
+        Intent intent = getIntent();
+        ArrayList<CharSequence> data = intent.getCharSequenceArrayListExtra("data");
+        for (CharSequence entry : data) {
+            TextView text = new TextView(this);
+            text.setText(entry);
+            text.setTextSize(20);
+
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT);
+            params.setMargins(40, 30, 0, 0);
+            text.setLayoutParams(params);
+
+            layout.addView(text);
+        }
+    }
+}
